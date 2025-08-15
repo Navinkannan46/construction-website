092e92ff0741c9290fa9ba003e78a140b02fda8b
Navinkannan46 <navinkannan46@gmail.com>
2025-08-15 14:23:54 +0530

    second commit

--- src/features/gallery/hooks/useFetchImages.tsx
+++ src/features/gallery/hooks/useFetchImages.tsx
@@ -3,20 +3,24 @@ import api from "../../../lib/api"
 import swal from 'sweetalert';
 const useFetchImages = () => {
     const [fetchDatas, setFetchDatas] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
     useEffect(() => {
 
         const fetchData = async () => {
+            setIsLoading(true)
             try {
                 const response = await api.get('/gallery')
                 setFetchDatas(response.data)
             } catch (error: any) {
                 swal(error.response?.data.message)
+            } finally {
+                setIsLoading(false)
             }
         }
         fetchData()
     }, [])
 
-    return { fetchDatas }
+    return { fetchDatas,isLoading }
 }
 
 export default useFetchImages
